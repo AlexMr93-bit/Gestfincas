22c66a6bc4000060bf0a8231e2c29a3bf6916b9b
Amr0177 <alexmr93estadistica@gmail.com>
2026-06-03 13:20:14 +0200

    Revert "Actualizacion de la app"
    This reverts commit edcc5205d677010570abb3df5bd36bb015e0d300.

--- config/hash.php
+++
@@ -1,8 +0,0 @@
-<?php
-//echo password_hash("casacasa", PASSWORD_BCRYPT);
-//echo password_hash("pepe", PASSWORD_BCRYPT);
-//echo password_hash("florcampo", PASSWORD_BCRYPT);
-//echo password_hash("magnolias", PASSWORD_BCRYPT);
-//echo password_hash("chilenita", PASSWORD_BCRYPT);
-echo password_hash("nati", PASSWORD_BCRYPT);
-?>
